6513a6b466327670a73ca4a9062df76bcb7794db
Alvaro <a@a.com>
2019-10-17 10:09:10 +0200

    Primer commit

---
+++ holamundo/src/ejerciciosUT1/Ejercicio3.java
@@ -0,0 +1,11 @@
+package ejerciciosUT1;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		int n, i;
+		System.out.println("Por favor, introduzca un número entre 1 y 100:")
+	}
+}
